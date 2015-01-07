902eb2de462413daf4deb396e47b1ad2c1906b9f
kr2558873 <kren@student.rcc.edu>
2015-01-06 20:07:17 -0800

    Signed-off-by: kr2558873 <kren@student.rcc.edu>

---
+++ Class_Lab/HelloWorld/main.cpp
@@ -0,0 +1,26 @@
+/* 
+   File:   main.cpp
+   Author: Kenley Ren
+   Created on January 6, 2015, 6:05 PM
+   Purpose: Our first program
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+
+int main(int argc, char** argv) {
+    //Output Hello World
+    cout<<"Hellow World"<<endl;
+    //Exit stage right!
+    return 0;
+}
+
